ea658ab8de7002370c645e305d0c80e4452698e9
yurim0628 <dbfla0303@naver.com>
2025-08-14 07:20:04 +0900

    feat: 도서 조회 서비스와 연동을 위한 BookInternalAdapter 클래스 추가
    - BookInternalPort 인터페이스의 findBooksByKeyword 메서드 구현해 실제 도서 조회 서비스와 연동

---
+++ src/main/java/org/example/booksearchservice/search/infrastructure/internal/BookInternalAdapter.java
@@ -0,0 +1,20 @@
+package org.example.booksearchservice.search.infrastructure.internal;
+
+import lombok.RequiredArgsConstructor;
+import org.example.booksearchservice.book.application.dto.BookPageResponse;
+import org.example.booksearchservice.book.application.service.BookQueryService;
+import org.example.booksearchservice.search.application.port.BookInternalPort;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BookInternalAdapter implements BookInternalPort {
+
+    private final BookQueryService bookQueryService;
+
+    @Override
+    public BookPageResponse findBooksByKeyword(String keyword, Pageable pageable) {
+        return bookQueryService.findBooksByKeyword(keyword, pageable);
+    }
+}
