5c5b67eebd6c95f23766a084b29d2cfec0d71d06
yurim0628 <dbfla0303@naver.com>
2025-08-13 21:25:55 +0900

    feat: BookEntity 기반 JpaRepository 인터페이스 추가

---
+++ src/main/java/org/example/booksearchservice/book/infrastructure/persistence/BookJpaRepository.java
@@ -0,0 +1,6 @@
+package org.example.booksearchservice.book.infrastructure.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookJpaRepository extends JpaRepository<BookEntity, Long> {
+}
