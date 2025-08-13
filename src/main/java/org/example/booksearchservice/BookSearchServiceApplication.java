d988fe32ab97d76f59a9d6fa6fd8189f43c07f1b
yurim0628 <dbfla0303@naver.com>
2025-08-13 13:17:49 +0900

    Create Book-Search-Service Project

---
+++ src/main/java/org/example/booksearchservice/BookSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.booksearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSearchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/booksearchservice/BookSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.booksearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
