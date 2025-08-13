fa837b24b6549520da8f75aaa68b3259e96bc05c
yurim0628 <dbfla0303@naver.com>
2025-08-14 06:52:52 +0900

    feat: 도서 목록 조회를 위해 LoadBookAdapter 클래스에 loadByKeyword 메서드 추가
    - LoadBookPort 인터페이스의 loadByKeyword 메서드를 구현함
    - BookJpaRepository 인터페이스에 findByKeyword 메서드를 추가해 단일 키워드로 도서 목록 조회

--- src/main/java/org/example/booksearchservice/book/infrastructure/persistence/BookJpaRepository.java
+++ src/main/java/org/example/booksearchservice/book/infrastructure/persistence/BookJpaRepository.java
@@ -1,6 +1,17 @@
 package org.example.booksearchservice.book.infrastructure.persistence;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookJpaRepository extends JpaRepository<BookEntity, Long> {
+
+    @Query("""
+        SELECT b
+        FROM BookEntity b
+        WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%'))
+           OR LOWER(b.subtitle) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        """)
+    Page<BookEntity> findByKeyword(String keyword, Pageable pageable);
 }
--- src/main/java/org/example/booksearchservice/book/infrastructure/persistence/LoadBookAdapter.java
+++ src/main/java/org/example/booksearchservice/book/infrastructure/persistence/LoadBookAdapter.java
@@ -3,6 +3,8 @@ package org.example.booksearchservice.book.infrastructure.persistence;
 import lombok.RequiredArgsConstructor;
 import org.example.booksearchservice.book.application.port.LoadBookPort;
 import org.example.booksearchservice.book.domain.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -16,6 +18,12 @@ public class LoadBookAdapter implements LoadBookPort {
     @Override
     public Optional<Book> loadById(Long id) {
         return bookJpaRepository.findById(id)
+                .map(BookEntity::toDomain);
+    }
+
+    @Override
+    public Page<Book> loadByKeyword(String keyword, Pageable pageable) {
+        return bookJpaRepository.findByKeyword(keyword, pageable)
                 .map(BookEntity::toDomain);
     }
 }
