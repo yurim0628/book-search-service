2fcdd31e3ed7db77d9e28cba0e6f484c58891527
yurim0628 <dbfla0303@naver.com>
2025-08-14 17:23:22 +0900

    feat: 복합 검색 요청을 처리하는 API 기능 구현

--- src/main/java/org/example/booksearchservice/search/presentation/dto/ComplexSearchRequest.java
+++ src/main/java/org/example/booksearchservice/search/presentation/dto/ComplexSearchRequest.java
@@ -2,8 +2,8 @@ package org.example.booksearchservice.search.presentation.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record SearchRequest(
+public record ComplexSearchRequest(
         @NotBlank(message = "검색어 입력은 필수입니다.")
-        String searchQuery
+        String query
 ) {
 }
--- src/main/java/org/example/booksearchservice/search/presentation/web/SearchController.java
+++ src/main/java/org/example/booksearchservice/search/presentation/web/SearchController.java
@@ -4,7 +4,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.booksearchservice.search.application.dto.SearchResponse;
 import org.example.booksearchservice.search.application.service.SearchService;
-import org.example.booksearchservice.search.presentation.dto.KeywordSearchRequest;
+import org.example.booksearchservice.search.presentation.dto.ComplexSearchRequest;
+import org.example.booksearchservice.search.presentation.dto.SimpleSearchRequest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -20,14 +21,25 @@ public class SearchController {
     private static final String  DEFAULT_PAGE = "0";
     private static final String DEFAULT_SIZE = "20";
 
-    @GetMapping
+    @GetMapping("/simple")
     public ResponseEntity<SearchResponse> searchBooks(
-            @Valid @ModelAttribute KeywordSearchRequest request,
+            @Valid @ModelAttribute SimpleSearchRequest request,
             @RequestParam(defaultValue = DEFAULT_PAGE) int page,
             @RequestParam(defaultValue = DEFAULT_SIZE) int size) {
 
         Pageable pageable = PageRequest.of(page, size);
         SearchResponse response = searchService.searchBooksByKeyword(request.keyword(), pageable);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping ("/complex")
+    public ResponseEntity<SearchResponse> searchBooksByComplexQuery(
+            @Valid @ModelAttribute ComplexSearchRequest request,
+            @RequestParam(defaultValue = DEFAULT_PAGE) int page,
+            @RequestParam(defaultValue = DEFAULT_SIZE) int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        SearchResponse response = searchService.searchBooksByComplexQuery(request.query(), pageable);
         return ResponseEntity.ok(response);
     }
 }
