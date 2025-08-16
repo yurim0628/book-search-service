ff302795984439cda1c4a047938b58e06a9ee499
yurim0628 <dbfla0303@naver.com>
2025-08-17 04:02:30 +0900

    refactor: NotSearchUseCase의 execute 메서드에 @Transactional 애너테이션 추가

--- src/main/java/org/example/booksearchservice/search/application/usecase/NotSearchUseCase.java
+++ src/main/java/org/example/booksearchservice/search/application/usecase/NotSearchUseCase.java
@@ -6,6 +6,7 @@ import org.example.booksearchservice.book.application.dto.BookPageResponse;
 import org.example.booksearchservice.search.application.port.BookInternalPort;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service("NOT")
@@ -15,6 +16,7 @@ public class NotSearchUseCase implements OperatorSearchUseCase {
     private final BookInternalPort bookInternalPort;
 
     @Override
+    @Transactional(readOnly = true)
     public BookPageResponse execute(String firstKeyword, String secondKeyword, Pageable pageable) {
         log.info("Executing NOT search with firstKeyword: [{}], excluding secondKeyword: [{}]",
                 firstKeyword, secondKeyword);
