a011275f3c232e1114a4b9f5e6abcc3d0c8be434
yurim0628 <dbfla0303@naver.com>
2025-08-17 03:51:32 +0900

    refactor: SearchExecutionTimeAspect의 실행 시간 로깅 개선

--- src/main/java/org/example/booksearchservice/search/application/aop/SearchExecutionTimeAspect.java
+++ src/main/java/org/example/booksearchservice/search/application/aop/SearchExecutionTimeAspect.java
@@ -1,6 +1,7 @@
 package org.example.booksearchservice.search.application.aop;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import static java.lang.System.currentTimeMillis;
 
+@Slf4j
 @Aspect
 @Component
 @RequiredArgsConstructor
@@ -17,19 +19,25 @@ public class SearchExecutionTimeAspect {
 
     @Around("execution(* org.example.booksearchservice.search.application.service.SearchService.*(..))")
     public Object recordExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[SearchExecution] [{}] started", joinPoint.getSignature().getName());
+
         long start = currentTimeMillis();
         Object result = joinPoint.proceed();
-        long executionTime = currentTimeMillis() - start;
+
+        long end = currentTimeMillis();
+        long executionTime = end - start;
+
+        log.info("[SearchExecution] [{} finished in [{}] ms", joinPoint.getSignature().getName(), executionTime);
 
         if (result instanceof SearchResponse response) {
             return SearchResponse.of(
-                response.searchQuery(),
-                response.pageInfo(),
-                response.books(),
-                SearchMetaData.of(
-                    executionTime,
-                    response.searchMetaData().strategy()
-                )
+                    response.searchQuery(),
+                    response.pageInfo(),
+                    response.books(),
+                    SearchMetaData.of(
+                            executionTime,
+                            response.searchMetaData().strategy()
+                    )
             );
         }
 
